4d771c1d134e192808cbe289e0d7144cc8212fcf
DediHumaedi <69851371+DediHumaedi@users.noreply.github.com>
2023-03-05 11:53:58 +0700

    Update downloader-tiktokmp4.js

--- plugins/downloader-tiktokmp4.js
+++ plugins/downloader-tiktokmp4.js
@@ -3,8 +3,6 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
     let url = args[0];
     let res = (await axios.get('https://api.akuari.my.id/downloader/tiktok?link=${url}')).data;
-    if (res.status != 200) throw res.message;
-    if (!res) throw res.message;
     
     let result = `⟐⟞⟚⟝⟮ *Title:* ⟯⟞⟚⟝⟐
 ┇⟣⟪ ${res.creator} ⟫⟢
